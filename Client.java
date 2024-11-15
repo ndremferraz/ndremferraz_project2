8463f8f28884150728443c329084d717d69854b2
Andre <andreferraz@ferrazbrasil.com.br>
2024-11-14 23:45:29 -0500

    Implemented Client.java to pass test 1

--- Client.java
+++ Client.java
@@ -1,3 +1,45 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+
+    // creates a Socket instance, initializes when constructor is called
+    private Socket clientsocket;
+
+    public Client(String localAddress, int portnumber) {
+        try {
+            this.clientsocket = new Socket(localAddress, portnumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handshake() {
+        try {
+
+            PrintWriter print = new PrintWriter(this.clientsocket.getOutputStream());
+            print.println("12345");
+            print.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void disconnect() {
+        try {
+
+            this.clientsocket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // getter methos for private field
+    public Socket getSocket() {
+        return clientsocket;
+    }
 
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,20 @@
+import java.net.ServerSocket;
+
 public class Server {
 
+    // creates a ServerSocket instance, initializes when constructor is called
+    private ServerSocket serversocket;
+
+    public Server(int portnumber) {
+        try {
+            this.serversocket = new ServerSocket(portnumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // getter methos for private field
+    public ServerSocket getServerSocket() {
+        return this.serversocket;
+    }
 }
